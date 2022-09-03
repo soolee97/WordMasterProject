b89ec6c76a7b810d3f23b3b6366d85ea5b4a8d89
soo <21700528@haneong.ac.kr>
2022-09-03 13:40:00 +0900

    Fourth Commit

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,10 +1,41 @@
 package org.example;
 
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+    ArrayList<Word> list ; //데이터 관리 타입
+    Scanner s = new Scanner(System.in) ;
+    /*
+    * => 난이도 (1,2,3) & 새 단어 입력 : 1 drive way
+    * 뜻 입력 : 차고 진입로
+    * 새 단어가 단어장에 추가되었습니다.
+    * */
+
+    //WordCRUD라는 생성자를 만들 때 객체화 하기 위한.
+    WordCRUD(Scanner s){
+        list = new ArrayList<>() ;
+        this.s = s ;
+    }
     @Override
     public Object add() {
-        return null;
+        // 사용자에게 입력 받는 부분 구현
+        System.out.print("=> 난이도 (1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt() ; //난이도 숫자 입력
+        String word = s.next() ; //영어 단어 입력
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine() ;
+
+        //(입력 받은 데이터 기준) word 라는 클래스 생성해거 객체로 return
+        return new Word(0, level, word, meaning) ;
+    }
+
+    public void addWord(){
+        // 리스트에 추가하는 부분 구현
+        Word one = (Word)add() ; //객체 생성 return type이 object 이기 때문에 (Word)
+        list.add(one) ;
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
     }
 
     @Override
--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -1,8 +1,55 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class WordManager {
     //CRUD 기능을 구현하는 전체적인 관리 역할
-    public void start(){
-        System.out.println("Word Manager !");
+    Scanner s = new Scanner(System.in) ;
+    WordCRUD wordCRUD ;
+    /*
+    *** 영단어 마스터 ***
+    *******************
+    1. 모든 단어 보기
+    2. 수준별 단어 보기
+    3. 단어 검색
+    4. 단어 추가
+    5. 단어 수정
+    6. 단어 삭제
+    7. 파일 저장
+    0. 나가기
+    *******************
+    => 원하는 메뉴는 ?
+    * */
+    WordManager(){
+        wordCRUD = new WordCRUD(s) ;
     }
+    public int selectMenu() {
+        System.out.print("*** 영단어 마스터 ***\n" +
+                "*******************\n" +
+                "1. 모든 단어 보기\n" +
+                "2. 수준별 단어 보기\n" +
+                "3. 단어 검색\n" +
+                "4. 단어 추가\n" +
+                "5. 단어 수정\n" +
+                "6. 단어 삭제\n" +
+                "7. 파일 저장\n" +
+                "0. 나가기\n" +
+                "*******************\n" +
+                " => 원하는 메뉴는 ? ");
+        return s.nextInt() ; //정수형태로 입력 받기
+    }
+    public void start(){
+        while(true) {
+            int menu = selectMenu();
+            if(menu == 0) break ;
+            if(menu == 4){
+                // 메뉴 추가
+                wordCRUD.addWord() ;
+            }
+            else if(menu ==1){
+                // 조회
+            }
+        }
+    }
+
 }
